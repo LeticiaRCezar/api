7ba11e4ae38f6c47da84ea3f183a646d7cf93504
LeticiaRCezar <leticia.cezar@aluno.senai.br>
2026-09-15 14:19:48 -0300

    Início

---
+++ src/test/java/api/ds/DsApplicationTests.java
@@ -0,0 +1,13 @@
+package api.ds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
